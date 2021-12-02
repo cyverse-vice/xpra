abf88feed8cbfb0a07a397ba4f1a9cb89633871e
Tyson Swetnam <tswetnam@cyverse.org>
2021-12-02 09:40:28 -0700

    updated harbor and builds

---
+++ orange/entry.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_zone_name": "iplant"}' | tee  > /home/orange/.irods/irods_environment.json
+
+exec xpra start --bind-tcp=0.0.0.0:9876 --html=on --start-child="python -m Orange.canvas"  --exit-with-children=no --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 1920x1080x24+32 -nolisten tcp -noreset" --file-transfer=on --pulseaudio=no --notifications=no --bell=no :100
