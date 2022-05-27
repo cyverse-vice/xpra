a6f6246297185ad2a3d95df1550a745eb6307bc3
Tyson Swetnam <tswetnam@cyverse.org>
2022-05-27 21:35:10 +0000

    updated

--- orange/entry.sh
+++ orange/entry.sh
@@ -2,4 +2,7 @@
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_zone_name": "iplant"}' | tee  > /home/orange/.irods/irods_environment.json
 
-exec xpra start --bind-tcp=0.0.0.0:9876 --html=on --start-child="python -m Orange.canvas"  --exit-with-children=no --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 1920x1080x24+32 -nolisten tcp -noreset" --file-transfer=on --pulseaudio=no --notifications=no --bell=no :100
+sudo chown -R orange:orange /run/user/1001
+sudo chown -R orange:orange /tmp
+
+exec xpra start --bind-tcp=0.0.0.0:9876 --html=on --start-child="gnome-terminal"  --exit-with-children=no --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 1920x1080x24+32 -nolisten tcp -noreset" --file-transfer=on --pulseaudio=no --notifications=no --bell=no :100
